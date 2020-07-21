3f9a00b62632db52d305f9011474a72bbc1a04b2
fis007 <schaffhauserfilip@gmail.com>
2020-07-21 11:57:54 +0800

    first commit

---
+++ Desktop/React Tutorial and Projects Course/todo-list2/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import uuid from 'uuid';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <TodoList />
+            <TodoInput />
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
