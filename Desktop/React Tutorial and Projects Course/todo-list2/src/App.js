ede7e3bae76ffadcbbd4bc14e7d03abd6cb0cc97
fis007 <schaffhauserfilip@gmail.com>
2020-08-14 17:40:28 +0800

    ready for deploy

--- Desktop/React Tutorial and Projects Course/todo-list2/src/App.js
+++ Desktop/React Tutorial and Projects Course/todo-list2/src/App.js
@@ -5,10 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-
-
-
-
+// showing vs-code github setup
+/////
 class App extends React.Component {
   render() {
     return (
